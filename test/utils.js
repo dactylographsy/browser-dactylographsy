8cac84e5a81de373fdc834f48dac2059b6c8b0fc
suǝʞǝǝpʇ <tobias.deekens@epost-dev.de>
2015-11-11 15:21:55 +0100

    Remove relying on array splatting in utils within specs.

--- test/utils.js
+++ test/utils.js
@@ -28,11 +28,17 @@ export class DOMUtil {
 
   removeAll() {
     let
-      nodes,
+      nodes = [],
       scripts = this.findAllJs(),
       styles = this.findAllCss();
 
-    nodes = [...styles, ...scripts];
+    for (var i = 0; i < styles.length; i++) {
+      nodes.push(styles[i]);
+    }
+
+    for (var i = 0; i < scripts.length; i++) {
+      nodes.push(scripts[i]);
+    }
 
     for (let node of nodes) {
       node.parentNode.removeChild(node);
